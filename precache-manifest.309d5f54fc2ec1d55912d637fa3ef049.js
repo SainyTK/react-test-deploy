7f0f936524a203e960876f7e5e0a998f3c0877a0
tanakorn0314 <tanakorn0412@gmail.com>
2019-02-13 19:42:47 +0700

    Updates

---
+++ precache-manifest.309d5f54fc2ec1d55912d637fa3ef049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test-deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60548fe2c0b7bf419b9e",
+    "url": "/react-test-deploy/static/js/runtime~main.60548fe2.js"
+  },
+  {
+    "revision": "440a075f77843ce100e9",
+    "url": "/react-test-deploy/static/js/main.440a075f.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/react-test-deploy/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "440a075f77843ce100e9",
+    "url": "/react-test-deploy/static/css/main.8cbf43a4.chunk.css"
+  },
+  {
+    "revision": "d961ce00d0d64fcea7dfd79811b62715",
+    "url": "/react-test-deploy/index.html"
+  }
+];
